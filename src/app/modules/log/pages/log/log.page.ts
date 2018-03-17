48632a35b2d65a56aa0d64d39e8b58b3e28ff3b8
Brandon Lee <brandonlee781@gmail.com>
2018-03-17 12:41:45 -0500

    Better observable pattern

--- src/app/modules/log/pages/log/log.page.ts
+++ src/app/modules/log/pages/log/log.page.ts
@@ -1,10 +1,12 @@
 // tslint:disable:component-class-suffix
 import { Component, OnInit } from '@angular/core';
 import { Apollo } from 'apollo-angular';
-import { GET_PROJECT_NAMES } from 'shared/queries';
+import { GET_PROJECT_NAMES } from 'shared/queries/index';
 import Project from 'modules/project/Project';
 import 'rxjs/add/operator/map';
-import { Observable } from 'apollo-client/util/Observable';
+import 'rxjs/add/operator/concat';
+import 'rxjs/add/observable/from';
+import { Observable } from 'rxjs/Observable';
 import { MatIconRegistry } from '@angular/material';
 import { DomSanitizer } from '@angular/platform-browser';
 
@@ -27,7 +29,7 @@ interface Link {
   styleUrls: ['./log.page.scss']
 })
 export class LogPage implements OnInit {
-  links: Link[];
+  links$: Observable<Link[]>;
   innerWidth: number;
   headerTitle: string;
   selectedProject: string;
@@ -46,35 +48,31 @@ export class LogPage implements OnInit {
   ngOnInit() {
     this.headerTitle = 'Recent Log Entries';
     this.selectedProject = 'recent';
-    this.apollo
+    const defaultLink = [{
+      name: 'Recent Log Entries',
+      id: 'recent',
+    }];
+    this.links$ = this.apollo
       .watchQuery<ProjectQuery>({ query: GET_PROJECT_NAMES })
       .valueChanges
-      .subscribe(q => {
-        const recent = [{
-          icon: '',
-          text: 'Recent Log Entries',
-          id: 'recent',
-          isSelected: true,
-        }];
-        this.links = recent.concat(
-          q.data.allProjects.projects.map(p => ({
-            icon: 'folder_open',
-            text: p.name,
-            id: p.id,
-            isSelected: false,
-          }))
-        );
-      });
+      .map(p => defaultLink.concat(p.data.allProjects.projects))
+      .map((arr: Project[]) => arr.map((proj: Project) => ({
+        icon: proj.id === 'recent' ? '' : 'folder_open',
+        isSelected: proj.id === 'recent' ? true : false,
+        text: proj.name,
+        id: proj.id,
+      })));
   }
 
   onLinkSelected(link) {
-    // TODO change selected project and refresh Log query
     this.headerTitle = link.text;
     this.selectedProject = link.id;
 
-    this.links = this.links.map(l => {
-      return Object.assign({}, l, {
-        isSelected: l.id === link.id ? true : false,
+    this.links$ = this.links$.map(links => {
+      return links.map(l => {
+        return Object.assign({}, l, {
+          isSelected: l.id === link.id ? true : false,
+        });
       });
     });
   }
