35d589032a1a09056b63a1bbafdd3350d2edce49
Brandon Lee <brandonlee781@gmail.com>
2018-03-29 21:17:24 -0500

    Directive to ensure times are valid

---
+++ src/app/shared/directives/log-time-validator.directive.spec.ts
@@ -0,0 +1,8 @@
+import { LogTimeValidatorDirective } from './log-time-validator.directive';
+
+describe('LogTimeValidatorDirective', () => {
+  it('should create an instance', () => {
+    const directive = new LogTimeValidatorDirective();
+    expect(directive).toBeTruthy();
+  });
+});
