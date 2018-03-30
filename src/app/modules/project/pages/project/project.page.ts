804bfe1955d3bd5c5da019390e0e754bb616fd5e
Brandon Lee <brandonlee781@gmail.com>
2018-03-29 21:19:47 -0500

    Other project page files

---
+++ src/app/modules/project/pages/project/project.page.ts
@@ -0,0 +1,13 @@
+// tslint:disable:component-class-suffix
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'bl-project',
+  templateUrl: './project.page.html',
+  styleUrls: ['./project.page.scss'],
+})
+export class ProjectPage implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
