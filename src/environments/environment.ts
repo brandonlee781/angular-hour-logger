2734083607911935e120b78f061061e886ef0278
Brandon Lee <brandonlee781@gmail.com>
2018-03-30 00:09:04 -0500

    Correct api url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,11 +2,9 @@
 // The build system defaults to the dev environment which uses `environment.ts`, but if you do
 // `ng build --env=prod` then `environment.prod.ts` will be used instead.
 // The list of which env maps to which file can be found in `.angular-cli.json`.
+import 'dotenv/config';
 
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:3000/graphql',
-  tempToken:
-    // tslint:disable-next-line
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImJyYW5kb25sZWU3ODFAZ21haWwuY29tIiwiaWQiOiI4ZjZiMTQ4Yy0wMWE2LTRhNzItYTJhMC1mNTE3MTUyOTE5OWYiLCJpYXQiOjE1MjA2Mzg0NTcsImV4cCI6MTUyMTg0ODA1N30.gEUh_cSEhDBeGS0aNIGj2zCGQEe40IVeYDoeLghaIVQ',
+  apiUrl: 'http://localhost:3000/api',
 };
