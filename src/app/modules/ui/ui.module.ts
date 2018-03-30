7d103f2ae5f5731526f7fae7eae644158c368584
Brandon Lee <brandonlee781@gmail.com>
2018-03-29 21:16:15 -0500

    Split out hamburger button for later use

--- src/app/modules/ui/ui.module.ts
+++ src/app/modules/ui/ui.module.ts
@@ -1,16 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
-
-import { SideNavComponent } from './components/side-nav/side-nav.component';
-import { SideNavBtnComponent } from './components/side-nav-btn/side-nav-btn.component';
 import { MaterialModule } from 'core/material.module';
 
+import { MenuToggleButtonComponent } from './components/menu-toggle-button/menu-toggle-button.component';
+import { SideNavBtnComponent } from './components/side-nav-btn/side-nav-btn.component';
+import { SideNavComponent } from './components/side-nav/side-nav.component';
+
 @NgModule({
-  imports: [
-    MaterialModule,
-    RouterModule,
+  imports: [MaterialModule, RouterModule],
+  declarations: [
+    SideNavComponent,
+    SideNavBtnComponent,
+    MenuToggleButtonComponent,
   ],
-  declarations: [SideNavComponent, SideNavBtnComponent],
-  exports: [ SideNavComponent, SideNavBtnComponent ],
+  exports: [SideNavComponent, SideNavBtnComponent, MenuToggleButtonComponent],
+  // providers: [ MatIconRegistry ]
 })
-export class UIModule { }
+export class UIModule {}
