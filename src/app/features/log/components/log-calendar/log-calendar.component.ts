6167211487c67bc2579242430687e1ae58799005
Brandon Lee <brandonlee781@gmail.com>
2018-04-02 15:02:14 -0500

    Remove colors

--- src/app/features/log/components/log-calendar/log-calendar.component.ts
+++ src/app/features/log/components/log-calendar/log-calendar.component.ts
@@ -6,21 +6,6 @@ import Log from 'features/log/Log';
 import { Subject } from 'rxjs/Subject';
 import { LOG_LIST_QUERY, LogListQuery } from 'shared/graphql/queries';
 
-const colors: any = {
-  red: {
-    primary: '#ad2121',
-    secondary: '#FAE3E3',
-  },
-  blue: {
-    primary: '#1e90ff',
-    secondary: '#D1E8FF',
-  },
-  yellow: {
-    primary: '#e3bc08',
-    secondary: '#FDF1BA',
-  },
-};
-
 @Component({
   selector: 'bl-log-calendar',
   templateUrl: './log-calendar.component.html',
