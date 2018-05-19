a5d4f329e200ff871d9e44fadc65ae171077ff19
Brandon Lee <brandonlee@gmail.com>
2018-05-19 10:17:06 -0500

    Loading spinner on log entries

--- src/app/features/log/components/log-list/log-list.component.ts
+++ src/app/features/log/components/log-list/log-list.component.ts
@@ -16,14 +16,24 @@ import { LOG_LIST_QUERY, LogListQuery } from '../../schema/queries';
   templateUrl: './log-list.component.html',
   styleUrls: ['./log-list.component.scss'],
 })
-export class LogListComponent implements OnInit {
+export class LogListComponent implements OnInit, OnChanges {
   @Input() logs: Log[];
   @Input() selectedProject: string = null;
+  @Input() loading = false;
   @Output() editLog = new EventEmitter<Log>();
   @Output() newLog = new EventEmitter<any>();
   @Output() loadMore = new EventEmitter<any>();
+  showEmptyState = false;
 
   constructor() {}
 
   ngOnInit() {}
+
+  ngOnChanges(changes: SimpleChanges) {
+    if (!this.loading && (!this.logs || !this.logs.length)) {
+      this.showEmptyState = true;
+    } else {
+      this.showEmptyState = false;
+    }
+  }
 }
